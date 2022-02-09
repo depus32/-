5b5e03dabfa2c49598e8193870dc3221c8e43eaa
Oleksandr <olexandrskliarov04097@gmail.com>
2022-02-09 18:01:09 +0200

    f

---
+++ code.js
@@ -0,0 +1,12 @@
+var swiper = new Swiper(".mySwiper", {
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+  const swiperr = new Swiper('.swiper', {
+    autoplay: {
+      delay: 2000,
+    },
+   });
+ 
